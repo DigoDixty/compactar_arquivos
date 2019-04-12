89f673b516056578577362d27a2f5593110192ce
USER <USER@DESKTOP-F3N3RKI>
2019-04-12 14:44:01 -0300

    Add search on subdirectories.

--- compactar_arquivos/compactar_arquivos/Program.cs
+++ compactar_arquivos/compactar_arquivos/Program.cs
@@ -13,46 +13,55 @@ namespace compactar_arquivos
 
         static void Main(string[] args)
         {
-            string file_log = "d:\\temp\\log_" + (DateTime.Now.ToString("yyyyMMdd_hhmmss")) + ".log";
-
-            escreve_log(file_log, "Inicio de console para compactação de arquivos.");
-
             //Parametros
             string v_diretorio = Properties.Settings.Default.diretorio_orig;
             string v_diretorio_tmp = v_diretorio + "\\temp";
             string v_extensao = Properties.Settings.Default.extensao_orig;
+            string file_log = v_diretorio + "\\log_" + (DateTime.Now.ToString("yyyyMMdd_hhmmss")) + ".txt";
 
+            escreve_log(file_log, "Inicio de console para compactação de arquivos.");
             escreve_log(file_log, "");
             escreve_log(file_log, "Diretório a procurar: " + v_diretorio);
             escreve_log(file_log, "Extensoes a procurar: " + v_extensao);
             escreve_log(file_log, "");
 
-            string[] ListArquivos = Directory.GetFiles(v_diretorio, v_extensao);
-
-            for (int i = 0; i < ListArquivos.Length; i++)
+            string[] ListDiretorios = Directory.GetDirectories(v_diretorio);
+            for (int cont_dir = 0; cont_dir < ListDiretorios.Length; cont_dir++)
             {
+                Console.WriteLine("");
+                Console.WriteLine("Diretorio encontrado: " + ListDiretorios[cont_dir]);
+                escreve_log(file_log, "");
+                escreve_log(file_log, "Diretorio encontrado: " + ListDiretorios[cont_dir]);
+                string nome_dir = ListDiretorios[cont_dir].Replace(v_diretorio, "");
+                string v_diretorio_sub = v_diretorio + nome_dir;
 
-                Console.WriteLine("Arquivo encontrado: " + ListArquivos[i]);
+                string[] ListArquivos = Directory.GetFiles(v_diretorio_sub, v_extensao);
 
-                escreve_log(file_log, "Arquivo encontrado: " + ListArquivos[i]);
-
-                string NomeArquivo = ListArquivos[i].Replace(v_diretorio, "");
-                string ext_file = Path.GetExtension(v_diretorio + NomeArquivo);
-                string NomeArquivo_zip = NomeArquivo.Replace(ext_file, ".zip");
-
-                if (!Directory.Exists(v_diretorio_tmp))
+                for (int conf_arq = 0; conf_arq < ListArquivos.Length; conf_arq++)
                 {
-                    Directory.CreateDirectory(v_diretorio_tmp);
+
+                    Console.WriteLine("Arquivo encontrado: " + ListArquivos[conf_arq]);
+                    escreve_log(file_log, "Arquivo encontrado: " + ListArquivos[conf_arq]);
+
+                    string NomeArquivo = ListArquivos[conf_arq].Replace(v_diretorio_sub, "");
+                    string ext_file = Path.GetExtension(v_diretorio_sub + NomeArquivo);
+                    string NomeArquivo_zip = NomeArquivo.Replace(ext_file, ".zip");
+
+                    if (!Directory.Exists(v_diretorio_tmp))
+                    {
+                        Directory.CreateDirectory(v_diretorio_tmp);
+                    }
+
+                    File.Move(v_diretorio_sub + NomeArquivo, v_diretorio_tmp + NomeArquivo);
+                    //escreve_log(file_log, "Arquivo movido para: " + v_diretorio_tmp + NomeArquivo);
+
+                    ZipFile.CreateFromDirectory(v_diretorio_tmp, v_diretorio_sub + NomeArquivo_zip);
+                    Console.WriteLine("Arquivo compactado: " + v_diretorio_sub + NomeArquivo_zip);
+                    escreve_log(file_log, "Arquivo compactado: " + v_diretorio_sub + NomeArquivo_zip);
+                    escreve_log(file_log, "");
+                    Directory.Delete(v_diretorio_tmp, true);
+
                 }
-
-                File.Move(v_diretorio + NomeArquivo, v_diretorio_tmp + NomeArquivo);
-                escreve_log(file_log, "Arquivo movido para: " + v_diretorio_tmp + NomeArquivo);
-
-                ZipFile.CreateFromDirectory(v_diretorio_tmp, v_diretorio + NomeArquivo_zip);
-                Console.WriteLine("Arquivo compactado: " + v_diretorio + NomeArquivo_zip);
-                escreve_log(file_log, "Arquivo compactado: " + v_diretorio + NomeArquivo_zip);
-
-                Directory.Delete(v_diretorio_tmp, true);
 
             }
 
